53e66b0d7eca87cd58fcae8f4711ee264c478cd9
Kenichi <minimi_ken1@yahoo.co.jp>
2024-06-17 11:46:35 +0900

    get todos

--- main.go
+++ main.go
@@ -18,20 +18,39 @@ func main() {
 
 	fmt.Println(models.Db)
 
-	// u := &models.User{}
-	// u.Name = "test"
-	// u.Email = "test@example.com"
-	// u.PassWord = "testtest"
+	u := &models.User{}
+	u.Name = "test"
+	u.Email = "test@example.com"
+	u.PassWord = "testtest"
+	fmt.Println(u)
+
+	u.CreateUser()
+
+	//現在の状態をプリントしてそこからupdateする
+
+	// u, _ := models.GetUser(1)
 	// fmt.Println(u)
 
-	// u.CreateUser()
-	u, _ := models.GetUser(1)
-	fmt.Println(u)
+	// u.Name = "Test2"
+	// u.Email = "test2@example.com"
+	// u.UpdateUser()
+	// u, _ = models.GetUser(1)
+	// fmt.Println(u)
 
-	u.Name = "Test2"
-	u.Email = "test2@example.com"
-	u.UpdateUser()
-	u, _ = models.GetUser(1)
-	fmt.Println(u)
+	// u.DeleteUser()
 
+	// user, _ := models.GetUser(2)
+	// user.CreateTodo("First Todo")
+
+	// t, _ := models.GetTodo(1)
+	// fmt.Println(t)
+
+	user, _ := models.GetUser(2)
+	user.CreateTodo("Second Todo")
+
+	todos, _ := models.GetTodos()
+	for _, v := range todos {
+		fmt.Println(v)
+
+	}
 }
