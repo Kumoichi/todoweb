24076e9c8b0daf282f48518fd1f2c3c23216c078
Kenichi <minimi_ken1@yahoo.co.jp>
2024-06-19 11:31:50 +0900

    login success

--- app/controllers/server.go
+++ app/controllers/server.go
@@ -16,11 +16,13 @@ func generateHTML(w http.ResponseWriter, data interface{}, filenames ...string) 
 	templates := template.Must(template.ParseFiles(files...))
 	templates.ExecuteTemplate(w, "layout", data)
 }
+
 func StartMainServer() error {
 	files := http.FileServer(http.Dir(config.Config.Static))
 	http.Handle("/static/", http.StripPrefix("/static/", files))
 	http.HandleFunc("/", top)
 	http.HandleFunc("/signup", signup)
 	http.HandleFunc("/login", login)
+	http.HandleFunc("/authenticate", authenticate)
 	return http.ListenAndServe(":"+config.Config.Port, nil)
 }
--- main.go
+++ main.go
@@ -3,22 +3,24 @@ package main
 import (
 	"fmt"
 	"log"
+	"todo_app/app/controllers"
 	"todo_app/app/models"
 )
 
 func main() {
 	fmt.Println(models.Db)
-	// controllers.StartMainServer()
+	controllers.StartMainServer()
 
 	user, _ := models.GetUserByEmail("test@example.com")
 	fmt.Println(user)
 
 	session, err := user.CreateSession()
 	if err != nil {
-		log.Println(err)
+		log.Fatalln(err)
 	}
 	fmt.Println(session)
 
 	valid, _ := session.CheckSession()
 	fmt.Println(valid)
+
 }
