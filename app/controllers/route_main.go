d58f79cd848a7db9b7c296e93dc3c611ae76c8c4
Kenichi <minimi_ken1@yahoo.co.jp>
2024-06-21 16:41:03 +0900

    todo update and todo delete

--- app/controllers/route_main.go
+++ app/controllers/route_main.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"todo_app/app/models"
@@ -80,24 +81,45 @@ func todoEdit(w http.ResponseWriter, r *http.Request, id int) {
 	}
 }
 
-// func todoUpdate(w http.ResponseWriter, r *http.Request, id int) {
-// 	sess, err := session(w, r)
-// 	if err != nil {
-// 		http.Redirect(w, r, "/login" 302)
-// 	} else {
-// 		err := r.ParseForm()
-// 		if err != nil {
-// 			log.Println(err)
-// 		}
-// 		user, err := sess.GetUserBySession()
-// 		if err != nil {
-// 			log.Println(err)
-// 		}
-// 		content := r.PostFormValue("content")
-// 		t := $models.Todo{ID: id, Content: content, UserID: user.ID}
-// 		if err := t.UpdateTodo(); err != nil {
-// 			log.Println(err)
-// 		}
-// 		http.Redirect(w, r, "todos", 302)
-// 	}
-// }
+func todoUpdate(w http.ResponseWriter, r *http.Request, id int) {
+	sess, err := session(w, r)
+	fmt.Println(id)
+	if err != nil {
+		http.Redirect(w, r, "/login", 302)
+	} else {
+		err := r.ParseForm()
+		if err != nil {
+			log.Println(err)
+		}
+		user, err := sess.GetUserBySession()
+		if err != nil {
+			log.Println(err)
+		}
+		content := r.PostFormValue("content")
+		t := models.Todo{ID: id, Content: content, UserID: user.ID}
+		if err := t.UpdateTodo(); err != nil {
+			log.Println(err)
+		}
+		http.Redirect(w, r, "/todos", 302)
+	}
+}
+
+func todoDelete(w http.ResponseWriter, r *http.Request, id int) {
+	sess, err := session(w, r)
+	if err != nil {
+		http.Redirect(w, r, "/login", 302)
+	} else {
+		_, err := sess.GetUserBySession()
+		if err != nil {
+			log.Println(err)
+		}
+		t, err := models.GetTodo(id)
+		if err != nil {
+			log.Println(err)
+		}
+		if err := t.DeleteTodo(); err != nil {
+			log.Println(err)
+		}
+		http.Redirect(w, r, "/todos", 302)
+	}
+}
--- app/controllers/server.go
+++ app/controllers/server.go
@@ -33,25 +33,36 @@ func session(w http.ResponseWriter, r *http.Request) (sess models.Session, err e
 	return sess, err
 }
 
-var validPath = regexp.MustCompile("^/todos/(edit|update)/([0-9])")
+// これはURLのパスの確認をしている。todo/edit/数字もしくはtodo/update/数字じゃないとValidPathとならない
+var validPath = regexp.MustCompile("^/todos/(edit|update|delete)/([0-9])")
+
+// 正規表現によるパスの検証:
+// parseURL 内で正規表現により、/todos/edit/数字 というパスの形式が保証されます。これにより、id の部分が整数であることが確認され、安全に strconv.Atoi(q[2]) を使って整数に変換できます。
 
 func parseURL(fn func(http.ResponseWriter, *http.Request, int)) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		// /todos/edit/1
+		fmt.Println("check2")
+		// /todos/edit/1をいれる。FindStringSubmatchは同じString部分を返すから、違う所があればNilが返ってくるんだと思う
 		q := validPath.FindStringSubmatch(r.URL.Path)
 		if q == nil {
+			fmt.Println("check3")
 			http.NotFound(w, r)
 			return
 		}
+		// AtoiはこのStringをintの整数に変える、q2は正規表現の数字の場所を表している
 		qi, err := strconv.Atoi(q[2])
 		if err != nil {
+			fmt.Println("check4")
 			http.NotFound(w, r)
 			return
 		}
-
+		fmt.Println("check5")
+		//いめーじとしてはreturn functionみたいなかんじ。
 		fn(w, r, qi)
 	}
 }
+
+//parseURL
 
 func StartMainServer() error {
 	files := http.FileServer(http.Dir(config.Config.Static))
@@ -65,6 +76,7 @@ func StartMainServer() error {
 	http.HandleFunc("/todos/new", todoNew)
 	http.HandleFunc("/todos/save", todoSave)
 	http.HandleFunc("/todos/edit/", parseURL(todoEdit))
-
+	http.HandleFunc("/todos/update/", parseURL(todoUpdate))
+	http.HandleFunc("/todos/delete/", parseURL(todoDelete))
 	return http.ListenAndServe(":"+config.Config.Port, nil)
 }
